35ad86e415ca88cc1a0c2f131dc05f62bdf5bd50
zyddnys <zyddnys@outlook.com>
2022-01-16 21:26:49 -0500

    more placeholder

---
+++ autotag/src/autotag.rs
@@ -0,0 +1,6 @@
+use crate::schema::{V1InferTagResponse, V1InferTagRequest};
+
+pub async fn v1_infer_tag_impl(request: V1InferTagRequest) -> Result<V1InferTagResponse, Box<dyn std::error::Error + Sync + Send>> {
+	todo!()
+}
+
--- autotag/src/route.rs
+++ autotag/src/route.rs
@@ -1,9 +1,13 @@
 
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError, http::StatusCode, web};
-use pvrustlib::{EmptyJSON, ServiceError};
+use actix_web::{HttpRequest, web};
+use pvrustlib::{ServiceError};
 
-use crate::schema::{V1InferTagRequest, V1InferTagResponse};
+use crate::{schema::{V1InferTagRequest, V1InferTagResponse}, autotag::*, SERVICE_NAME};
 
-pub async fn v1_infer_tag(request: HttpRequest, body: actix_web::web::Json<V1InferTagRequest>) -> Result<web::Json<V1InferTagResponse>, ServiceError> {
-	todo!()
+pub async fn v1_infer_tag(_request: HttpRequest, body: actix_web::web::Json<V1InferTagRequest>) -> Result<web::Json<V1InferTagResponse>, ServiceError> {
+	let ret = v1_infer_tag_impl(body.0).await;
+	match ret {
+		Ok(result) => Ok(web::Json(result)),
+		Err(e) => Err(ServiceError::from_dyn_error(SERVICE_NAME, e)),
+	}
 }
